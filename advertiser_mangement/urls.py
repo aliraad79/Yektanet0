9b059c99cda95d952a6a52129d9ce7289a294f35
aliraad79 <aliraad79@gmail.com>
2020-12-01 16:40:11 +0330

    change urls to click

--- advertiser_mangement/urls.py
+++ advertiser_mangement/urls.py
@@ -5,6 +5,6 @@ from .views import show_ads, count_ad_view, create_ad
 app_name = 'advertiser_mangement'
 urlpatterns = [
     path('ad/', show_ads, name='show-ad'),
-    path('ad/<int:ad_id>/', count_ad_view, name='count-ad-view'),
+    path('click/<int:ad_id>/', count_ad_view, name='count-ad-view'),
     path('ad/create/', create_ad , name='create-ad'),
 ]
