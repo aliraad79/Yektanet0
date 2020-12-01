12d38328b7337dd2019a1e023cb2a7332aaa2d06
aliraad79 <aliraad79@gmail.com>
2020-12-01 23:42:08 +0330

    make urls more readble

--- advertiser_mangement/urls.py
+++ advertiser_mangement/urls.py
@@ -3,10 +3,8 @@ from .views import show_ads, count_ad_click ,create_ad
 from django.conf.urls.static import static
 from django.conf import settings
 
-app_name = 'advertiser_mangement'
 urlpatterns = [
-    path('ad/', show_ads, name='show-ad'),
-    path('click/<int:ad_id>/', count_ad_click, name='count-ad-view'),
+    path('', show_ads, name='show-ad-all'),
+    path('click/<int:ad_id>/', count_ad_click, name='count-ad-click'),
     path('ad/create/', create_ad , name='create-ad'),
-] + static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
