5c57c95a401cf1c51a5c3777f5e650711a2b623f
aliraad79 <aliraad79@gmail.com>
2020-12-01 17:55:24 +0330

    complete CreateAdForm

--- advertiser_mangement/forms.py
+++ advertiser_mangement/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from advertiser_mangement.models import Ad
+class CreateAdForm(forms.ModelForm):
+    class Meta:
+        model = Ad
+        fields = ['advertiser','title', 'img', 'link']
 
-class CreateAdForm(forms.Form):
-    pass
