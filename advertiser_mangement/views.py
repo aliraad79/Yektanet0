7152fe43e5d23120e9ed41df1f13c480c02c1248
aliraad79 <aliraad79@gmail.com>
2020-12-01 16:37:50 +0330

    complete show_ads function

--- advertiser_mangement/views.py
+++ advertiser_mangement/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Advertiser, Ad
 
 # Create your views here.
 
 
 def show_ads(request):
-    return HttpResponse(request)
+    advertisers = Advertiser.objects.all()
+    context = {'advertisers':advertisers}
+    return render(request,'advertiser_mangement/ads.html',context=context)
 
 
 def count_ad_view(request, ad_id):
