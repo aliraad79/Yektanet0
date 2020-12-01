f3ae4275e3fa50185a2ac54e28e83ef51dfc29f7
aliraad79 <aliraad79@gmail.com>
2020-12-01 23:26:54 +0330

    add static to handle image in urls

--- advertiser_mangement/urls.py
+++ advertiser_mangement/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import show_ads, count_ad_click ,create_ad
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'advertiser_mangement'
 urlpatterns = [
     path('ad/', show_ads, name='show-ad'),
     path('click/<int:ad_id>/', count_ad_click, name='count-ad-view'),
     path('ad/create/', create_ad , name='create-ad'),
-]
+] + static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
