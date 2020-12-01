16eafc03ed68e5941217c16d4dbf875728cd5081
aliraad79 <aliraad79@gmail.com>
2020-12-01 17:19:36 +0330

    change count_ad_view to count_ad_click

--- advertiser_mangement/urls.py
+++ advertiser_mangement/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import show_ads, count_ad_view, create_ad
+from .views import show_ads, count_ad_click ,create_ad
 
 
 app_name = 'advertiser_mangement'
 urlpatterns = [
     path('ad/', show_ads, name='show-ad'),
-    path('click/<int:ad_id>/', count_ad_view, name='count-ad-view'),
+    path('click/<int:ad_id>/', count_ad_click, name='count-ad-view'),
     path('ad/create/', create_ad , name='create-ad'),
 ]
