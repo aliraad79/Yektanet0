5ef2e7e01b67e324a03a718bebed532031a82fb8
aliraad79 <aliraad79@gmail.com>
2020-12-03 17:29:03 +0330

    add crispy to create ad and advertiser forms

--- Yektanet/settings.py
+++ Yektanet/settings.py
@@ -28,7 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'advertiser_mangement.apps.AdvertiserMangementConfig'
+    'advertiser_mangement.apps.AdvertiserMangementConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
