58b0fad1bc8649fed2fa082c566fc3f552ad9487
aliraad79 <aliraad79@gmail.com>
2020-12-05 10:49:00 +0330

    add click and view models to admin

--- advertiser_management/admin.py
+++ advertiser_management/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Ad, Advertiser
+from .models import Ad, Advertiser, View, Click
 
 admin.site.register(Ad)
 admin.site.register(Advertiser)
+admin.site.register(View)
+admin.site.register(Click)
