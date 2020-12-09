28ee268bcb77074e135e8cdcad4edb086608f8a2
aliraad79 <aliraad79@gmail.com>
2020-12-09 11:10:40 +0330

    changing approve is now an admin action

--- advertiser_management/admin.py
+++ advertiser_management/admin.py
@@ -2,14 +2,26 @@ from django.contrib import admin
 from .models import Ad, Advertiser
 
 
+def approve_ad(modeladmin, request, queryset):
+    queryset.update(approve='True')
+
+
+def disapprove_ad(modeladmin, request, queryset):
+    queryset.update(approve='False')
+
+
+approve_ad.short_description = "Mark selected Ad approved"
+disapprove_ad.short_description = "Mark selected Ad disapproved"
+
+
 @admin.register(Ad)
 class AdAdmin(admin.ModelAdmin):
     list_display = ('title', 'advertiser', 'approve')
     list_filter = ('approve',)
     search_fields = ('title',)
-    list_editable = ('approve',)
+    actions = [approve_ad, disapprove_ad]
 
 
 @admin.register(Advertiser)
 class AdvertiserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('name', 'id')
