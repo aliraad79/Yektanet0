4ba728fcdab9b1d99a8c329feccfea411183487c
aliraad79 <aliraad79@gmail.com>
2020-12-09 11:11:10 +0330

    move rest_framework from middleware to install apps

--- Yektanet/settings.py
+++ Yektanet/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'advertiser_management.apps.AdvertiserManagementConfig',
     'crispy_forms',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'rest_framework',
 ]
 
 TEMPLATES = [
