eca147aa32e43ff844261783f34506364ebf3e0d
aliraad79 <aliraad79@gmail.com>
2020-12-10 13:14:03 +0330

    remove api url

--- Yektanet/urls.py
+++ Yektanet/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('advertiser_management.urls')),
-    path('api/',include('restapi.urls')),
 ]
