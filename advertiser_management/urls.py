7b3a8eaccca6b661b3611e92f088f61d7ab6310d
aliraad79 <aliraad79@gmail.com>
2020-12-10 18:34:31 +0330

    change click url

--- advertiser_management/urls.py
+++ advertiser_management/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import AdListView, AdViewSet, AdvertiserViewSet
+
+from .views import AdViewSet, AdvertiserViewSet
 from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework.routers import DefaultRouter
@@ -10,5 +11,5 @@ router.register(r'advertiser', AdvertiserViewSet)
 ad_click = AdViewSet.as_view({'get': 'click'})
 
 urlpatterns = [
-                  path('ad/click/<int:ad_id>/', ad_click, name='click-add'),
+                  path('ad/click/<int:pk>/', ad_click),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + router.urls
